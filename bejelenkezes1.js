10820a4bb7caf7086d7b2804f03d4a27bba8f44e
KOVDOM <94444372+KOVDOM@users.noreply.github.com>
2022-05-30 20:53:02 +0200

    Add files via upload

--- bejelenkezes1.js
+++ bejelenkezes1.js
@@ -51,20 +51,20 @@ document.getElementById('login').onsubmit = function (event) {
 function renderUsers(){
   var unknownHTML ='';
   for (var unknown of state){
-    unknownHTML =`<input type="number" name="idemail" class="form-control" /><br><button id="idemail">Katt</button>`
+    unknownHTML =`<input type="number" name="idemail" class="form-control" /><br><button type="submit" id="idemail">Katt</button>`
   }
   document.getElementById('user-list-container').innerHTML=`<ul class="list-group">`+unknownHTML+`</ul>`;
 }
 //<li class="list-group-item">${unknown.name}</li>
 document.getElementById('idemail').onclick=function(event){
-  //event.preventDefault();
-  var password = event.target.elements.password.value;
+  event.preventDefault();
+  var name = event.target.elements.name.value;
   var body = JSON.stringify({
     name: name
   })
 };
 
-fetch('https://reqres.in/api/unknown', {
+fetch('https://reqres.in/api/unknown/2', {
     method: 'POST',
     body: body,
     headers: {
@@ -78,3 +78,25 @@ fetch('https://reqres.in/api/unknown', {
       }
       return response.json();
     })
+    .then(function (unknownPage) {
+      console.log(unknownPage);
+    state = unknownPage.data;
+    renderUsers2();
+    //document.getElementById('login').style.display="none";
+    })
+    .catch(function (error) {
+      console.log(error);
+      alert(error);
+    })
+  console.log(email);
+  console.log(password);
+  console.log(body);
+;
+
+    function renderUsers2(){
+      var unknownHTML2 ='';
+      for (var unknown of state){
+        unknownHTML2 =`<li class="list-group-item">${unknown.name}</li>`
+      }
+      document.getElementById('user-list-container2').innerHTML2=`<ul class="list-group">`+unknownHTML2+`</ul>`;
+    };
